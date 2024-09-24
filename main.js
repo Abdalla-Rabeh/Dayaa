6c43cae5a9d72fb52801288a8f7be815a606f9bb
Abdalla-Rabeh <abdallahcodeing@gmail.com>
2024-09-25 02:09:49 +0300

    test

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ function setTab(value) {
     const urlInput = document.getElementById('urlInput');
     const textInput = document.getElementById('textInput');
     const phoneInput = document.getElementById('phoneInput');
+    const vcardInput = document.getElementById('vcardInput');
     const wifiInput = document.getElementById('wifiInput');
     urlInput.style.display = 'none';
     textInput.style.display = 'none';
     phoneInput.style.display = 'none';
     wifiInput.style.display = 'none';
+    vcardInput.style.display = 'none';
     if (value === 'url') {
         urlInput.style.display = 'block';
     }else if (value === 'text') {
@@ -15,6 +17,9 @@ function setTab(value) {
     else if (value === 'phone') {
         phoneInput.style.display = 'block';
     }
+    else if (value === 'vcard') {
+        vcardInput.style.display = 'block';
+    }    
     else if (value === 'wifi') {
         wifiInput.style.display = 'block';
     }
